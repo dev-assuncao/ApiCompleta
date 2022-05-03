dededc2c6e9b0d03f6420f3aed55b9355ea26a21
dev-assuncao <davisson.assuncao@protonmail.com>
2022-05-03 20:57:34 -0300

    Creating controller main and provider, connection with database and using automapper

---
+++ MyFirstAPI/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        //validacao de notificacoes de erros
+
+        // validacao de ModelState
+
+        // validacao da operacao de negocios
+    }
+}
--- MyFirstAPI/src/DevIO.Api/Startup.cs
+++ MyFirstAPI/src/DevIO.Api/Startup.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AutoMapper;
+using DevIO.Api.Configuration;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevIO.Api
 {
@@ -25,7 +24,11 @@ namespace DevIO.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MeuDbContext>(options => options.UseMySql(Configuration.GetConnectionString("MeuDbContext")));
+
+            services.AddAutoMapper(typeof(Startup));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
